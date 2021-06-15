b777681ba09623668bb73cc40474b087983e9fb1
hyyrim <realD1006@gmail.com>
2021-06-15 23:46:58 +0900

    AOP

---
+++ SpringPrj09/src/com/test/spr/CalculatorBeforeAdvice.java
@@ -0,0 +1,35 @@
+/*==================================
+	CalculatorBeforeAdvice.java
+	- Before Advice 구성
+==================================*/
+
+package com.test.spr;
+
+import java.lang.reflect.Method;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.aop.MethodBeforeAdvice;
+
+public class CalculatorBeforeAdvice implements MethodBeforeAdvice
+{
+	
+	@Override
+	public void before(Method arg0, Object[] arg1, Object arg2) throws Throwable
+	{
+		// 매개변수는 건들지않아도 상관없다.
+		// MethodBeforeAdvice 를 implements 하겠다하면 
+		// 스프링이 알아서 Before Advice 로 삽입해준다.
+		// 여기서 중요한건, 주업무를 실행하기 전에 스프링이 Before Advice 를 만들어준다는 것.
+		// 물론 만들어졌으면 이 클래스를 기반으로 스프링이 객체를 운용할 수 있게끔 해줘야함.
+		
+		Log log = LogFactory.getLog(this.getClass());
+		log.info("Before Advice 실행--------------------");
+		log.info("주 업무 실행 전에 수행되어야 하는 작업");
+		log.info("--------------------Before Advice 실행");
+		
+		
+		
+	}
+	
+}
